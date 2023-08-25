269c22c5cd1d61a01087878388dc0333d2396299
AB <41185891+theCodingPhoenix@users.noreply.github.com>
2023-08-25 22:11:52 +0100

    Install sass and add styles on homepage

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/ingredient-list.scss";
 
 const IngredientsList = ({ ingredients }) => {
   return (
-    <div>
+    <div className="ingredient-list">
       {ingredients.map((ingredient) => {
         const { idIngredient: id, strIngredient: name } = ingredient;
+        const imgSrc = `https:/www.themealdb.com/images/ingredients/${name}-Small.png`;
         return (
           <Link to={`/recipe/ingredient/${name}`} key={id}>
-            <p>{name}</p>
+            <div className="ingredient-list__ingredient">
+              <img src={imgSrc} title={name} alt={name} />
+              <p className="ingredient-list__name">{name}</p>
+            </div>
           </Link>
         );
       })}
