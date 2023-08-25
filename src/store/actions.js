2cdd31d83007f2f05fbabcffe9e87a59586f14c5
AB <41185891+theCodingPhoenix@users.noreply.github.com>
2023-08-25 15:39:16 +0100

    Import and url corrected

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,20 +3,15 @@ import {
   FETCH_INGREDIENTS_REQUEST,
   FETCH_INGREDIENTS_SUCCESS,
   FETCH_INGREDIENTS_ERROR,
-  //   FETCH_INGREDIENT_MEALS_REQUEST,
-  //   FETCH_INGREDIENT_MEALS_SUCCESS,
-  //   FETCH_INGREDIENT_MEALS_ERROR,
-  //   FETCH_MEAL_REQUEST,
-  //   FETCH_MEAL_SUCCESS,
-  //   FETCH_MEAL_ERROR,
 } from "./actionTypes";
+
+const baseURL = "https://www.themealdb.com/api/json/v1/1/";
+const ingredientListURL = "list.php?i=list";
 
 export const fetchIngredients = async (dispatch) => {
   try {
     dispatch({ type: FETCH_INGREDIENTS_REQUEST });
-    const response = await axios.get(
-      "www.themealdb.com/api/json/v1/1/list.php?i=list"
-    );
+    const response = await axios.get(baseURL + ingredientListURL);
     dispatch({ type: FETCH_INGREDIENTS_SUCCESS, payload: response.data.meals });
   } catch (error) {
     dispatch({ type: FETCH_INGREDIENTS_ERROR, payload: error.message });
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,7 @@ import {
   FETCH_INGREDIENTS_REQUEST,
   FETCH_INGREDIENTS_SUCCESS,
   FETCH_INGREDIENTS_ERROR,
-} from "./actions";
+} from "./actionTypes";
 
 export const recipeReducer = (state, action) => {
   switch (action.type) {
