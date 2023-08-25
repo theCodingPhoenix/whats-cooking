305900bdbd85a8631b756e8a4de9bbc68b96e300
AB <41185891+theCodingPhoenix@users.noreply.github.com>
2023-08-25 20:13:27 +0100

    Add simple search on home page

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchRecipes from "./SearchRecipes";
+
+function Header() {
+  return (
+    <header>
+      <h3>What's cooking, good looking!</h3>
+      <SearchRecipes></SearchRecipes>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useRecipeContext } from "../store/context";
 import IngredientsList from "../components/IngredientsList";
+import Header from "../components/Header";
+import RecipeList from "../components/RecipeList";
 
 const Home = () => {
-  const { ingredients, ingredientsLoading } = useRecipeContext();
+  const { ingredients, ingredientsLoading, recipes, recipesLoading } =
+    useRecipeContext();
 
   return (
     <div>
+      <Header></Header>
+      {recipesLoading ? (
+        "Loading"
+      ) : (
+        <RecipeList recipes={recipes}> </RecipeList>
+      )}
       {ingredientsLoading ? (
         "Loading"
       ) : (
