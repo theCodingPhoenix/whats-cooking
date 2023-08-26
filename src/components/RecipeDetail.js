22443218f6d1977d2660df3e10f90d727abd80c6
AB <41185891+theCodingPhoenix@users.noreply.github.com>
2023-08-26 08:52:10 +0100

    Show recipe data and setup initial styles for page

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,8 +1,69 @@
 import React from "react";
+import "../styles/recipe.scss";
 
 function RecipeDetail({ recipeInfo }) {
-  console.log(recipeInfo);
-  return <div>{recipeInfo.strInstructions} </div>;
+  let recipe;
+  let ingredients = [];
+  let measures = [];
+  let lisOfIngredients = [];
+
+  // lets build up an array of objects that will contain
+  // ingredients required for the recipe and the measures
+  if (recipeInfo && recipeInfo[0]) {
+    recipe = { ...recipeInfo[0] };
+    for (let keyProp in recipe) {
+      // build the ingredients array
+      if (keyProp.includes("strIngredient")) {
+        if (recipe[keyProp]) {
+          ingredients.push(recipe[keyProp]);
+        }
+      } else if (keyProp.includes("strMeasure")) {
+        // build the measurements array
+        if (recipe[keyProp]) {
+          measures.push(recipe[keyProp]);
+        }
+      }
+    }
+
+    if (ingredients.length > 0) {
+      // build the list of ingredients with ingrdient and their measurement
+      ingredients.forEach(function (currentIngredient, index) {
+        let currentMeasure = ";";
+        if (measures.length > 0 && measures[index]) {
+          currentMeasure = measures[index];
+        }
+        lisOfIngredients.push({
+          ingredient: currentIngredient,
+          measure: currentMeasure,
+        });
+      });
+    }
+  }
+
+  return (
+    <>
+      {recipe ? (
+        <div className="recipe">
+          <p>{recipe.strMeal}</p>
+          <img
+            src={recipe?.strMealThumb}
+            alt={recipe.strMeal}
+            title={recipe.strMeal}
+          />
+          <ul>
+            {lisOfIngredients.map((requiredIngredient, index) => (
+              <li key={index}>
+                {requiredIngredient.measure} {requiredIngredient.ingredient}
+              </li>
+            ))}
+          </ul>
+          <div>{recipe.strInstructions}</div>
+        </div>
+      ) : (
+        <p>Not Found</p>
+      )}
+    </>
+  );
 }
 
 export default RecipeDetail;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useRecipeContext } from "../store/context";
 import IngredientsList from "../components/IngredientsList";
-import Header from "../components/Header";
-import RecipeList from "../components/RecipeList";
+// import Header from "../components/Header";
+// import RecipeList from "../components/RecipeList";
 
 const Home = () => {
   const { ingredients, ingredientsLoading } = useRecipeContext();
